9e0433c5d0f469ce6d8ea0f839364e4d430a267b
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2023-07-01 20:14:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'https://bloggie-app-posts.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://bloggie-app-posts.herokuapp.com/' });
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'https://blogie-server-zhxv.vercel.app/'});
+// const API = axios.create({ baseURL: 'https://blogie-server-zhxv.vercel.app/'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
